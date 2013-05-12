69e918d643eb7d343499b6e5b3fa84e542872caa
Sarunas Valaskevicius <rakatan@gmail.com>
2013-05-12 12:42:54 +0100

    add virtual destructors

--- lib/cpgf/include/cpgf/ginterface.h
+++ lib/cpgf/include/cpgf/ginterface.h
@@ -33,6 +33,7 @@ struct IObject
    virtual uint32_t G_API_CC unused_queryInterface(void *, void *) = 0;
    virtual uint32_t G_API_CC addReference() = 0;
    virtual uint32_t G_API_CC releaseReference() = 0;
+   virtual ~IObject(){}
 };
 
 
