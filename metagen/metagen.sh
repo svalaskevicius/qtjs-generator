cf7009e0b5d4caa420ff0c1cf48f8bfa2354502d
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-05-20 21:05:14 +0100

    prepare space for adding qtWidgets

--- metagen/metagen.sh
+++ metagen/metagen.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
-java -cp "../lib/cpgf/tools/metagen/tool/bin:../lib/cpgf/tools/metagen/js.jar" metagen --config config.js --xml xml/index.xml
+java -cp "../lib/cpgf/tools/metagen/tool/bin:../lib/cpgf/tools/metagen/js.jar" metagen --config config.js --xml xml/qtCore/index.xml
 
