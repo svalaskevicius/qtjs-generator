3793bf54cc13bc3c048df50ee66a96ac803177f7
Sarunas Valaskevicius <rakatan@gmail.com>
2013-07-15 16:52:19 +0100

    add logging example back

--- examples/basic.js
+++ examples/basic.js
@@ -54,6 +54,30 @@ function createMainWindow() {
 }
 
 (function(){
+  var pt = new qt.QPoint(1, 2);
+  var log = new qt.QMessageLogger();
+
+  log.debug()
+    ._opLeftShift(qt.QString.fromLatin1(pt.toString()))
+    ._opLeftShift(new qt.QString("asd"))
+    ._opLeftShift(123)
+    ._opLeftShift(true)
+    ._opLeftShift(pt.y())
+    ._opLeftShift(qt.qHash("test"))
+  ;
+
+  var obj = new qt.QObject();
+  qt.connect(obj, 'objectNameChanged(const QString &)', function(name){
+    log.debug()
+    ._opLeftShift(new qt.QString("NAME CHANGED! "+name))
+    ;
+  });
+
+  obj.setObjectName(new qt.QString("asd"));
+
+})();
+
+(function(){
   var w = createMainWindow();
   w.show();
 })();
