69c06a406959d285af8aa044f14d9e47b67fcc0b
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-08-03 23:42:36 +0100

    refactor main.cpp

--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -41,52 +41,61 @@
 using namespace cpgf;
 using namespace std;
 
+namespace {
+
 int __exitCode = 1;
 void setExitCode(int code) {
     __exitCode = code;
 }
 
-int main(int argc, char * argv[])
+void registerQt(GDefineMetaNamespace &define) 
 {
-    QApplication app(argc, argv);
-    {
-
-	GDefineMetaNamespace define = GDefineMetaNamespace::declare("qt");
     meta_qtcore::registerMain_QtCore(define);
     meta_qtwidgets::registerMain_QtWidgets(define);
 
     qtjs_binder::QtSignalConnectorBinder::reset(new qtjs_binder::QtSignalConnector());
     define._method("connect", &qtjs_binder::QtSignalConnectorBinder::connect);
     define._method("setExitCode", &setExitCode );
+}
 
-	const char * fileName = "main.js";
-	
-	if(argc > 1) {
-		fileName = argv[1];
-	}
+bool executeJs(const char *fileName) 
+{
+	GDefineMetaNamespace define = GDefineMetaNamespace::declare("qt");
+	registerQt(define);
 
-	cout << "Running.." << endl;
     GScopedPointer<GScriptRunner> runner;
     GScopedInterface<IMetaService> service(createDefaultMetaService());
     runner.reset(createV8ScriptRunner(service.get()));
     GScopedInterface<IScriptObject> scriptObject(runner->getScripeObject());
 
     scriptObject->bindCoreService("cpgf", NULL);
-
-
     GScopedInterface<IMetaClass> metaClass(static_cast<IMetaClass *>(metaItemToInterface(define.getMetaClass())));
     scriptObject->bindClass("qt", metaClass.get());
 
-    if(!runner->executeFile(fileName)) {
-        cout << "Failed to execute " << fileName << ", maybe it doesn't exist?" << endl;
-    }
-
+    bool ret = runner->executeFile(fileName);
 
     while (!v8::V8::IdleNotification()); // run GC
     clearV8DataPool();
     qtjs_binder::QtSignalConnectorBinder::reset();
     
     service->releaseReference();
+
+    return ret;
+}
+
+} // namespace
+
+int main(int argc, char * argv[])
+{
+    QApplication app(argc, argv);
+
+	const char * fileName = "main.js";
+	if(argc > 1) {
+		fileName = argv[1];
+	}
+
+    if (!executeJs(fileName)) {
+        cerr << "Failed to execute " << fileName << ", maybe it doesn't exist?" << endl;
     }
 
     v8::V8::Dispose();
