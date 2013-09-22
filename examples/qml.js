89cca06f97fedc23e244ff0d1edee2dd31e04365
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-09-22 22:52:25 +0100

    working version with pseudo dynamic type

--- examples/qml.js
+++ examples/qml.js
@@ -1,12 +1,38 @@
+cpgf.import("cpgf", "builtin.core");
+
 (function() {
     "use strict";
 
-//    var engine = new qt.QQmlEngine();
-//    var component = new qt.QQmlComponent(engine, new qt.QString("qml/main.qml"));
-//    component.create();
-    var window = new qt.QQuickView();
-    window.setSource(qt.QUrl.fromLocalFile(new qt.QString("qml/main.qml")));
+    try {
+    var engine = new qt.QQmlEngine();
+//    engine.rootContext().setContextProperty(new qt.QString('keygen'), qt.newKeyGenerator());
+    var component = new qt.QQmlComponent(engine, new qt.QString("qml/main.qml"));
+    if (!component.isReady() ) {
+      throw component.errorString();
+    }
+
+    qt.QCoreApplication.instance().connect(engine, '2quit()', '1quit()');
+
+    var topLevel = component.create();
+    var window = cpgf.cast(topLevel, qt.QQuickWindow);
+    var surfaceFormat = window.requestedFormat();
+    window.setFormat(surfaceFormat);
     window.show();
+
+} catch(err) {
+    if (err instanceof qt.QString) {
+      var log = new qt.QMessageLogger();
+      log.critical()._opLeftShift(err);
+      qt.setExitCode(1);
+      return;
+    }
+    throw err;
+}
+
+
+//    var window = new qt.QQuickView();
+//    window.setSource(qt.QUrl.fromLocalFile(new qt.QString("qml/main.qml")));
+//    window.show();
 
     qt.setExitCode(
        qt.QCoreApplication.instance().exec()
--- src/runner/main.cpp
+++ src/runner/main.cpp
@@ -1,6 +1,6 @@
 
 #include <QApplication>
-#include <QMainWindow>
+#include <QStringList>
 
 #include "cpgf/gmetadefine.h"
 #include "cpgf/scriptbind/gscriptbindutil.h"
@@ -22,6 +22,14 @@
 
 #include "QtSignalConnector.h"
 
+
+#include <QQmlComponent>
+
+
+#include <QFile>
+#include <QProcess>
+#include <QDebug>
+
 using namespace cpgf;
 using namespace std;
 
@@ -40,6 +48,391 @@ void setExitCode(int code)
     __exitCode = code;
 }
 
+struct qt_meta_stringdata_KeyGenerator_t {
+    QByteArrayData data[13];
+    char stringdata[138];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_KeyGenerator_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_KeyGenerator_t qt_meta_stringdata_KeyGenerator = {
+    {
+QT_MOC_LITERAL(0, 0, 12),
+QT_MOC_LITERAL(1, 13, 11),
+QT_MOC_LITERAL(2, 25, 0),
+QT_MOC_LITERAL(3, 26, 12),
+QT_MOC_LITERAL(4, 39, 15),
+QT_MOC_LITERAL(5, 55, 17),
+QT_MOC_LITERAL(6, 73, 12),
+QT_MOC_LITERAL(7, 86, 7),
+QT_MOC_LITERAL(8, 94, 11),
+QT_MOC_LITERAL(9, 106, 4),
+QT_MOC_LITERAL(10, 111, 5),
+QT_MOC_LITERAL(11, 117, 8),
+QT_MOC_LITERAL(12, 126, 10)
+    },
+    "KeyGenerator\0typeChanged\0\0typesChanged\0"
+    "filenameChanged\0passphraseChanged\0"
+    "keyGenerated\0success\0generateKey\0type\0"
+    "types\0filename\0passphrase\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_KeyGenerator[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       4,   52, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       5,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   44,    2, 0x05,
+       3,    0,   45,    2, 0x05,
+       4,    0,   46,    2, 0x05,
+       5,    0,   47,    2, 0x05,
+       6,    1,   48,    2, 0x05,
+
+ // slots: name, argc, parameters, tag, flags
+       8,    0,   51,    2, 0x0a,
+
+ // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,    7,
+
+ // slots: parameters
+    QMetaType::Void,
+
+ // properties: name, type, flags
+       9, QMetaType::QString, 0x00495103,
+      10, QMetaType::QStringList, 0x00495001,
+      11, QMetaType::QString, 0x00495103,
+      12, QMetaType::QString, 0x00495103,
+
+ // properties: notify_signal_id
+       0,
+       1,
+       2,
+       3,
+
+       0        // eod
+};
+class KeyGenerator : public QObject
+{
+public:
+    KeyGenerator();
+    ~KeyGenerator();
+
+    QString type();
+    void setType(const QString &t);
+
+    QStringList types();
+
+    QString filename();
+    void setFilename(const QString &f);
+
+    QString passphrase();
+    void setPassphrase(const QString &p);
+
+public:
+    void generateKey();
+
+private:
+    QString _type;
+    QString _filename;
+    QString _passphrase;
+    QStringList _types;
+
+
+
+
+
+public:
+
+void static qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        qDebug() << "static invoke mete method: "<<_id;
+        KeyGenerator *_t = static_cast<KeyGenerator *>(_o);
+        switch (_id) {
+        case 0: _t->typeChanged(); break;
+        case 1: _t->typesChanged(); break;
+        case 2: _t->filenameChanged(); break;
+        case 3: _t->passphraseChanged(); break;
+        case 4: _t->keyGenerated((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 5: _t->generateKey(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        qDebug() << "static indexof method: "<<func;
+        {
+            typedef void (KeyGenerator::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&KeyGenerator::typeChanged)) {
+                *result = 0;
+            }
+        }
+        {
+            typedef void (KeyGenerator::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&KeyGenerator::typesChanged)) {
+                *result = 1;
+            }
+        }
+        {
+            typedef void (KeyGenerator::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&KeyGenerator::filenameChanged)) {
+                *result = 2;
+            }
+        }
+        {
+            typedef void (KeyGenerator::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&KeyGenerator::passphraseChanged)) {
+                *result = 3;
+            }
+        }
+        {
+            typedef void (KeyGenerator::*_t)(bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&KeyGenerator::keyGenerated)) {
+                *result = 4;
+            }
+        }
+    }
+}
+
+const static QMetaObject staticMetaObject;
+
+
+const QMetaObject *metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *qt_metacast(const char *_clname)
+{
+        qDebug() << "metacast: "<<_clname;
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_KeyGenerator.stringdata))
+        return static_cast<void*>(const_cast< KeyGenerator*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+virtual int qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        qDebug() << "metacall: "<<_id;
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        qDebug() << "register method argument metatype: "<<_id;
+        if (_id < 6)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 6;
+    }
+#ifndef QT_NO_PROPERTIES
+      else if (_c == QMetaObject::ReadProperty) {
+        qDebug() << "read property: "<<_id;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< QString*>(_v) = type(); break;
+        case 1: *reinterpret_cast< QStringList*>(_v) = types(); break;
+        case 2: *reinterpret_cast< QString*>(_v) = filename(); break;
+        case 3: *reinterpret_cast< QString*>(_v) = filename(); break;
+        }
+        _id -= 4;
+    } else if (_c == QMetaObject::WriteProperty) {
+        qDebug() << "write property: "<<_id;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: setType(*reinterpret_cast< QString*>(_v)); break;
+        case 2: setFilename(*reinterpret_cast< QString*>(_v)); break;
+        case 3: setPassphrase(*reinterpret_cast< QString*>(_v)); break;
+        }
+        _id -= 4;
+    } else if (_c == QMetaObject::ResetProperty) {
+        _id -= 4;
+    } else if (_c == QMetaObject::QueryPropertyDesignable) {
+        _id -= 4;
+    } else if (_c == QMetaObject::QueryPropertyScriptable) {
+        _id -= 4;
+    } else if (_c == QMetaObject::QueryPropertyStored) {
+        _id -= 4;
+    } else if (_c == QMetaObject::QueryPropertyEditable) {
+        _id -= 4;
+    } else if (_c == QMetaObject::QueryPropertyUser) {
+        _id -= 4;
+    } else if (_c == QMetaObject::RegisterPropertyMetaType) {
+        if (_id < 4)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 4;
+    }
+#endif // QT_NO_PROPERTIES
+    return _id;
+}
+
+// SIGNAL 0
+void typeChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, 0);
+}
+
+// SIGNAL 1
+void typesChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, 0);
+}
+
+// SIGNAL 2
+void filenameChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, 0);
+}
+
+// SIGNAL 3
+void passphraseChanged()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, 0);
+}
+
+// SIGNAL 4
+void keyGenerated(bool _t1)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 4, _a);
+}
+};
+
+const QMetaObject KeyGenerator::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_KeyGenerator.data,
+      qt_meta_data_KeyGenerator,  qt_static_metacall, 0, 0}
+};
+
+
+/*
+int KeyGenerator::qt_metacall(QMetaObject::Call call, int id, void **data)
+{
+    id = QObject::qt_metacall(call, id, data);
+    qDebug() << id;
+    if (id == -1 || call != QMetaObject::InvokeMetaMethod) {
+        return id;
+    }
+
+    return -1;
+}
+
+*/
+KeyGenerator::KeyGenerator()
+    : _type("rsa"), _types{"dsa", "ecdsa", "rsa", "rsa1"}
+{
+}
+
+KeyGenerator::~KeyGenerator()
+{
+}
+
+QString KeyGenerator::type()
+{
+    return _type;
+}
+
+void KeyGenerator::setType(const QString &t)
+{
+    // Check for valid type.
+    if (!_types.contains(t))
+        return;
+
+    if (t != _type) {
+        _type = t;
+        emit typeChanged();
+    }
+}
+
+QStringList KeyGenerator::types()
+{
+    return _types;
+}
+
+QString KeyGenerator::filename()
+{
+    return _filename;
+}
+
+void KeyGenerator::setFilename(const QString &f)
+{
+    if (f != _filename) {
+        _filename = f;
+        emit filenameChanged();
+    }
+}
+
+QString KeyGenerator::passphrase()
+{
+    return _passphrase;
+}
+
+void KeyGenerator::setPassphrase(const QString &p)
+{
+    if (p != _passphrase) {
+        _passphrase = p;
+        emit passphraseChanged();
+    }
+}
+
+void KeyGenerator::generateKey()
+{
+    qDebug() << "generating: "<< _passphrase;
+        emit keyGenerated(false);
+        return;
+
+    // Sanity check on arguments
+    if (_type.isEmpty() or _filename.isEmpty() or
+        (_passphrase.length() > 0 and _passphrase.length() < 5)) {
+        emit keyGenerated(false);
+        return;
+    }
+
+    // Remove key file if it already exists
+    if (QFile::exists(_filename)) {
+        QFile::remove(_filename);
+    }
+
+    // Execute ssh-keygen -t type -N passphrase -f keyfileq
+    QProcess *proc = new QProcess;
+    QString prog = "ssh-keygen";
+    QStringList args{"-t", _type, "-N", _passphrase, "-f", _filename};
+    proc->start(prog, args);
+    proc->waitForFinished();
+    emit keyGenerated(proc->exitCode() == 0);
+    delete proc;
+}
+
+
+
+
+
+
+
+QObject * newKeyGenerator() {
+    return new KeyGenerator();
+}
+
+
+
+
 void registerQt(GDefineMetaNamespace &define)
 {
     meta_qtcore::registerMain_QtCore(define);
@@ -51,6 +444,7 @@ void registerQt(GDefineMetaNamespace &define)
     define._method("connect", &qtjs_binder::QtSignalConnectorBinder::connect);
     define._method("programArguments", &programArguments);
     define._method("setExitCode", &setExitCode);
+    define._method("newKeyGenerator", &newKeyGenerator);
 }
 
 bool executeJs(const char *fileName)
@@ -81,6 +475,8 @@ bool executeJs(const char *fileName)
 int main(int argc, char * argv[])
 {
     QApplication app(argc, argv);
+
+    qmlRegisterType<KeyGenerator>("com.ics.demo", 1, 0, "KeyGenerator");
 
     const char * fileName = "main.js";
     if (argc > 1) {
