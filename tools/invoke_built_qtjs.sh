a1a7498162212d51af76427f390d479ba87c6262
Sarunas Valaskevicius <rakatan@gmail.com>
2013-10-30 20:39:34 +0000

    support symlinks

--- tools/invoke_built_qtjs.sh
+++ tools/invoke_built_qtjs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MYDIR=$(dirname "$0")
+MYDIR=$(dirname "$(readlink -f "$0")")
 TOPDIR="$MYDIR/.."
 
 export LD_LIBRARY_PATH="$TOPDIR/lib/cpgf/lib:$TOPDIR/build/core:$TOPDIR/build/widgets:$TOPDIR/build/qml:$TOPDIR/build/gui"
