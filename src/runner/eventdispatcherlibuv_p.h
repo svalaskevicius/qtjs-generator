36bf625f184c2ce959b5cd1451e49a1f6287976a
Sarunas Valaskevicius <rakatan@gmail.com>
2013-11-03 20:01:21 +0000

    add timer structures

--- src/runner/eventdispatcherlibuv_p.h
+++ src/runner/eventdispatcherlibuv_p.h
@@ -17,12 +17,17 @@ struct SocketCallbacks {
 };
 
 void uv_socket_watcher(uv_poll_t* handle, int status, int events);
+void uv_timer_watcher(uv_timer_t* handle, int status);
 
 struct LibuvApi {
     virtual ~LibuvApi() {}
     virtual int uv_poll_init(uv_loop_t* loop, uv_poll_t* handle, int fd);
     virtual int uv_poll_start(uv_poll_t* handle, int events, uv_poll_cb cb);
     virtual int uv_poll_stop(uv_poll_t* handle);
+
+    virtual int uv_timer_init(uv_loop_t*, uv_timer_t* handle);
+    virtual int uv_timer_start(uv_timer_t* handle, uv_timer_cb cb, uint64_t timeout, uint64_t repeat);
+    virtual int uv_timer_stop(uv_timer_t* handle);
 };
 
 class EventDispatcherLibUvPrivate {
