94ad14c1f32a1cffc5bda225387af06854f75220
Sarunas Valaskevicius <rakatan@gmail.com>
2013-11-03 12:44:44 +0000

    initialise uv polling handle

--- tests/spec/eventdispatcherlibuv.h
+++ tests/spec/eventdispatcherlibuv.h
@@ -7,24 +7,31 @@
 #include <QSocketNotifier>
 
 
-
 MOCK_BASE_CLASS( MockedLibuvApi, qtjs::LibuvApi ) {
     MOCK_METHOD(uv_poll_init, 3);
+    MOCK_METHOD(uv_poll_start, 3);
 };
 
 TEST_CASE("libuv based event dispatcher") {
 
-    SECTION("registerSocketNotifier initialises libuv handle for the given fd") {
+    SECTION("registerSocketNotifier initialises libuv handle for the given read fd") {
 
-        qtjs::EventDispatcherLibUvPrivate dispatcher;
+        uv_poll_t *registeredHandle, *usedHandle;
 
-        MockedLibuvApi api;
-        MOCK_EXPECT( api.uv_poll_init ).once( ).with( mock::any, mock::any, mock::equal(19) ).returns(0);
-        api.uv_poll_init(nullptr, nullptr, 19);
+        MockedLibuvApi *api = new MockedLibuvApi();
+        MOCK_EXPECT( api->uv_poll_init ).once()
+                .with( mock::equal(uv_default_loop()), mock::retrieve(registeredHandle), mock::equal(19) )
+                .returns(0);
 
-//        dispatcher.registerSocketNotifier(19, QSocketNotifier::Read);
+        MOCK_EXPECT( api->uv_poll_start ).once()
+                .with( mock::retrieve(usedHandle), mock::equal(UV_READABLE), mock::equal(&qtjs::uv_socket_watcher) )
+                .returns(0);
 
-//        REQUIRE(1);
+        qtjs::EventDispatcherLibUvPrivate dispatcher(api);
+        dispatcher.registerSocketNotifier(19, QSocketNotifier::Read);
+
+        REQUIRE( registeredHandle );
+        REQUIRE( registeredHandle == usedHandle );
     }
 
 }
