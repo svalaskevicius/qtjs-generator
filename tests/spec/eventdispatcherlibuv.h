8f254a91ced705c07405fa3670b1d97cc48fe2a1
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-11-03 12:55:02 +0000

    init uv poller for qsocketnotifier write requests

--- tests/spec/eventdispatcherlibuv.h
+++ tests/spec/eventdispatcherlibuv.h
@@ -12,27 +12,51 @@ MOCK_BASE_CLASS( MockedLibuvApi, qtjs::LibuvApi ) {
     MOCK_METHOD(uv_poll_start, 3);
 };
 
-TEST_CASE("libuv based event dispatcher") {
+namespace {
 
-    SECTION("registerSocketNotifier initialises libuv handle for the given read fd") {
+struct pollInitMocker {
+    uv_poll_t *registeredHandle, *usedHandle;
 
-        uv_poll_t *registeredHandle, *usedHandle;
-
-        MockedLibuvApi *api = new MockedLibuvApi();
+    pollInitMocker(MockedLibuvApi *api, int fd, int type) {
         MOCK_EXPECT( api->uv_poll_init ).once()
-                .with( mock::equal(uv_default_loop()), mock::retrieve(registeredHandle), mock::equal(19) )
+                .with( mock::equal(uv_default_loop()), mock::retrieve(registeredHandle), mock::equal(fd) )
                 .returns(0);
 
         MOCK_EXPECT( api->uv_poll_start ).once()
-                .with( mock::retrieve(usedHandle), mock::equal(UV_READABLE), mock::equal(&qtjs::uv_socket_watcher) )
+                .with( mock::retrieve(usedHandle), mock::equal(type), mock::equal(&qtjs::uv_socket_watcher) )
                 .returns(0);
+    }
+
+    void checkHandles() {
+        REQUIRE( registeredHandle );
+        REQUIRE( registeredHandle == usedHandle );
+    }
+};
+
+}
+
+TEST_CASE("libuv based event dispatcher")
+{
+    SECTION("registerSocketNotifier initialises libuv handle for the given read fd")
+    {
+        MockedLibuvApi *api = new MockedLibuvApi();
+        pollInitMocker pollMocker(api, 19, UV_READABLE);
 
         qtjs::EventDispatcherLibUvPrivate dispatcher(api);
         dispatcher.registerSocketNotifier(19, QSocketNotifier::Read);
 
-        REQUIRE( registeredHandle );
-        REQUIRE( registeredHandle == usedHandle );
+        pollMocker.checkHandles();
+    }
+
+    SECTION("registerSocketNotifier initialises libuv handle for the given write fd")
+    {
+        MockedLibuvApi *api = new MockedLibuvApi();
+        pollInitMocker pollMocker(api, 20, UV_WRITABLE);
+
+        qtjs::EventDispatcherLibUvPrivate dispatcher(api);
+        dispatcher.registerSocketNotifier(20, QSocketNotifier::Write);
+
+        pollMocker.checkHandles();
     }
 
 }
-
