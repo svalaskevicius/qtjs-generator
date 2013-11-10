d72f36313a45bb407544d3a3eeef110f94cbdd1c
Sarunas Valaskevicius <rakatan@gmail.com>
2013-11-10 22:04:16 +0000

    move cpgf binder to an earlier loading stage

--- tools/debug_built_qtjs.sh
+++ tools/debug_built_qtjs.sh
@@ -3,9 +3,9 @@
 MYDIR=$(dirname "$0")
 TOPDIR="$MYDIR/.."
 
-export LD_LIBRARY_PATH="$TOPDIR/lib/cpgf/lib:$TOPDIR/build/core:$TOPDIR/build/widgets:$TOPDIR/build/qml:$TOPDIR/build/gui"
+export LD_LIBRARY_PATH="$TOPDIR/lib/cpgf/lib:$TOPDIR/build/src/core:$TOPDIR/build/src/widgets:$TOPDIR/build/src/qml:$TOPDIR/build/src/gui:$TOPDIR/lib/node/deps/v8/out/native/lib.target/"
 
-gdb --args "$TOPDIR/build/runner/qtjs" "$@"
+gdb --args "$TOPDIR/build/src/runner/qtjs" "$@"
 
 exit $?
 
