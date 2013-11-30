f50bb6c42334e9f3e599d06cc77cf1bfabc3d074
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-11-30 13:35:43 +0000

    update memcheck helper script

--- tools/memcheck_built_qtjs.sh
+++ tools/memcheck_built_qtjs.sh
@@ -3,10 +3,10 @@
 MYDIR=$(dirname "$0")
 TOPDIR="$MYDIR/.."
 
-export LD_LIBRARY_PATH="$TOPDIR/lib/cpgf/lib:$TOPDIR/build/core:$TOPDIR/build/widgets:$TOPDIR/build/qml:$TOPDIR/build/gui"
+export LD_LIBRARY_PATH="$TOPDIR/lib/cpgf/lib:$TOPDIR/build/src/core:$TOPDIR/build/src/widgets:$TOPDIR/build/src/qml:$TOPDIR/build/src/gui:$TOPDIR/lib/node/deps/v8/out/native/lib.target/"
 
-valgrind --tool=memcheck --track-origins=yes --leak-check=full --show-reachable=yes --suppressions="$MYDIR"/.valgrind.suppress --gen-suppressions=all --log-file="$TOPDIR/build/memcheck.log" "$TOPDIR/build/runner/qtjs" "$@" &
-valgrind --tool=memcheck --track-origins=yes --leak-check=full --show-reachable=yes --suppressions="$MYDIR"/.valgrind.suppress --gen-suppressions=all --xml=yes --xml-file="$TOPDIR/build/memcheck.xml" "$TOPDIR/build/runner/qtjs" "$@" &
+valgrind --tool=memcheck --track-origins=yes --leak-check=full --show-reachable=yes --suppressions="$MYDIR"/.valgrind.suppress --gen-suppressions=all --log-file="$TOPDIR/build/memcheck.log" "$TOPDIR/build/src/runner/qtjs" "$@" &
+# valgrind --tool=memcheck --track-origins=yes --leak-check=full --show-reachable=yes --suppressions="$MYDIR"/.valgrind.suppress --gen-suppressions=all --xml=yes --xml-file="$TOPDIR/build/memcheck.xml" "$TOPDIR/build/src/runner/qtjs" "$@" &
 
 wait
 
