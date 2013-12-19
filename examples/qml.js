1df2225e69e7d8ee7d4a66b095084b257c040b0c
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-12-19 14:38:26 +0000

    update qml example

--- examples/qml.js
+++ examples/qml.js
@@ -1,98 +1,139 @@
 "use strict"
 
 qt.include("core.js")
-
 cpgf.import("cpgf", "builtin.core");
 
-        (function() {
+var MySyntaxHighlighter = cpgf.cloneClass(qt.QSyntaxHighlighterWrapper);
+MySyntaxHighlighter.highlightBlock = function($this , text ) {
+    var myClassFormat = new qt.QTextCharFormat();
+    myClassFormat.setFontWeight(qt.QFont.Bold);
+    myClassFormat.setForeground(new qt.QBrush(new qt.QColor(qt.darkMagenta)));
+    var pattern = new qt.QString("\\bMy[A-Za-z]+\\b");
 
-            (function(){
-                var b = new qt.DynamicMetaObjectBuilder()
-                b.setClassName("KeyGenerator")
-                b.setInit(function ($this) {
-                    $this.setProperty("type",
-                                      new qt.QVariant(new qt.QString("rsa")))
-                    var aa = new qt.QStringList()
-                    aa._opLeftShift(new qt.QString("rsa"))
-                    aa._opLeftShift(new qt.QString("dsa"))
-                    $this.setProperty("types", new qt.QVariant(aa))
-                })
-                var aa = new qt.QStringList()
-                aa._opLeftShift(new qt.QString("success"))
+    var expression = new qt.QRegExp(pattern);
+    var index = expression.indexIn(text);
+    while (index >= 0) {
+        var length = expression.matchedLength();
+        $this.setFormat(index, length, myClassFormat);
+        index = expression.indexIn(text, index + length);
+    }
+};
 
-                b.addSignal("keyGenerated(bool)", aa)
-                b.addProperty("type", "QString")
-                b.addProperty("types", "QStringList")
-                b.addProperty("passphrase", "QString")
-                b.addProperty("filename", "QString")
 
-                b.addSlot('generateKey()', function ($this) {
-                    var log = new qt.QMessageLogger()
+(function() {
 
-                    log.debug()._opLeftShift(
-                                new qt.QString("invoking generate! "))._opLeftShift(
-                                $this.property("passphrase").toString())
+    var highlighter;
+    (function(){
+        var b = new qt.DynamicMetaObjectBuilder()
+        b.setClassName("KeyGenerator")
+        b.setInit(function ($this) {
+            $this.setProperty("type",
+                              new qt.QVariant(new qt.QString("rsa")))
+            var aa = new qt.QStringList()
+            aa._opLeftShift(new qt.QString("rsa"))
+            aa._opLeftShift(new qt.QString("dsa"))
+            $this.setProperty("types", new qt.QVariant(aa))
+        })
+        var aa = new qt.QStringList()
+        aa._opLeftShift(new qt.QString("success"))
 
-                    $this.emitSignal("keyGenerated(bool)",
-                                     new qt.QVariant(true))
-                })
+        b.addSignal("keyGenerated(bool)", aa)
+        b.addProperty("type", "QString")
+        b.addProperty("types", "QStringList")
+        b.addProperty("passphrase", "QString")
+        b.addProperty("filename", "QString")
 
-                qt.finalizeAndRegisterMetaObjectBuilderToQml(b,
-                                                             "com.ics.demo", 1,
-                                                             0, "KeyGenerator")
-            })();
+        b.addSlot('generateKey()', function ($this) {
+            var log = new qt.QMessageLogger()
 
-            (function(){
-                var b = new qt.DynamicMetaObjectBuilder()
-                b.setClassName("IntIncrementer")
-                b.setInit(function ($this) {
-                    $this.setProperty("value", new qt.QVariant(0))
-                })
-                var aa = new qt.QStringList()
-                aa._opLeftShift(new qt.QString("newValue"))
-                b.addSignal("incremented(int)", aa)
-                b.addProperty("value", "int")
-                b.addSlot('increment()', function ($this) {
-                    $this.setProperty('value',
-                                      new qt.QVariant($this.property(
-                                                          "value").toInt() + 1))
-                    $this.emitSignal("incremented(int)",
-                                     new qt.QVariant($this.property(
-                                                         "value").toInt()))
-                })
+            log.debug()._opLeftShift(
+                        new qt.QString("invoking generate! "))._opLeftShift(
+                        $this.property("passphrase").toString())
 
-                qt.finalizeAndRegisterMetaObjectBuilderToQml(b, "com.ics.demo",
-                                                             1, 0,
-                                                             "IntIncrementer")
-            })();
+            $this.emitSignal("keyGenerated(bool)",
+                             new qt.QVariant(true))
+        })
 
-            try {
-                var engine = new qt.QQmlEngine()
-                //    engine.rootContext().setContextProperty(new qt.QString('keygen'), qt.newKeyGenerator());
-                var component = new qt.QQmlComponent(engine,
-                                                     qt.makeIncludePathAbsolute(
-                                                         new qt.QString("qml/main.qml")))
-                if (!component.isReady()) {
-                    throw component.errorString()
+        qt.finalizeAndRegisterMetaObjectBuilderToQml(b,
+                                                     "com.ics.demo", 1,
+                                                     0, "KeyGenerator")
+    })();
+
+    (function(){
+        var b = new qt.DynamicMetaObjectBuilder()
+        b.setClassName("IntIncrementer")
+        b.setInit(function ($this) {
+            $this.setProperty("value", new qt.QVariant(0))
+        })
+        var aa = new qt.QStringList()
+        aa._opLeftShift(new qt.QString("newValue"))
+        b.addSignal("incremented(int)", aa)
+        b.addProperty("value", "int")
+        b.addSlot('increment()', function ($this) {
+            $this.setProperty('value',
+                              new qt.QVariant($this.property(
+                                                  "value").toInt() + 1))
+            $this.emitSignal("incremented(int)",
+                             new qt.QVariant($this.property(
+                                                 "value").toInt()))
+        })
+
+        qt.finalizeAndRegisterMetaObjectBuilderToQml(b, "com.ics.demo",
+                                                     1, 0,
+                                                     "IntIncrementer")
+    })();
+
+    try {
+        var engine = new qt.QQmlEngine()
+        //    engine.rootContext().setContextProperty(new qt.QString('keygen'), qt.newKeyGenerator());
+        var component = new qt.QQmlComponent(engine,
+                                             qt.makeIncludePathAbsolute(
+                                                 new qt.QString("qml/main.qml")))
+        if (!component.isReady()) {
+            throw component.errorString()
+        }
+
+        qt.QCoreApplication.instance().connect(engine, '2quit()',
+                                               '1quit()')
+
+        var topLevel = component.create();
+        var window = cpgf.cast(topLevel, qt.QQuickWindow);
+        var surfaceFormat = window.requestedFormat();
+        window.setFormat(surfaceFormat);
+
+
+        var findChild = function (obj, name) {
+            var begin = obj.children().cbegin();
+            var end = obj.children().cend();
+            for (var o = begin; o._opNotEqual(end); o._opIncSuffix()) {
+                if (o._opDerefer().objectName()._opEqual(name)) {
+                    return o._opDerefer();
                 }
-
-                qt.QCoreApplication.instance().connect(engine, '2quit()',
-                                                       '1quit()')
-
-                var topLevel = component.create()
-                var window = cpgf.cast(topLevel, qt.QQuickWindow)
-                var surfaceFormat = window.requestedFormat()
-                window.setFormat(surfaceFormat)
-                window.show()
-            } catch (err) {
-                if (err instanceof qt.QString) {
-                    var log = new qt.QMessageLogger()
-                    log.critical()._opLeftShift(err)
-                    qt.setExitCode(1)
-                    return
+                var ret = findChild(o._opDerefer(), name);
+                if (ret) {
+                    return ret;
                 }
-                throw err
             }
+            return null;
+        }
+        var edit = findChild(topLevel, "editor");
+        if (edit) {
+            edit = qt.objectFromVariant(edit.property("textDocument"));
+            if (edit) {
+              edit = cpgf.cast(edit, qt.QQuickTextDocument);
+              highlighter = new MySyntaxHighlighter(edit.textDocument());
+            }
+        }
+        window.show()
+    } catch (err) {
+        if (err instanceof qt.QString) {
+            var log = new qt.QMessageLogger()
+            log.critical()._opLeftShift(err)
+            qt.setExitCode(1)
+            return
+        }
+        throw err
+    }
 
-            qt.setExitCode(qt.QCoreApplication.instance().exec())
-        })()
+    qt.setExitCode(qt.QCoreApplication.instance().exec())
+})()
--- examples/qml/main.qml
+++ examples/qml/main.qml
@@ -1,9 +1,9 @@
 
 // SSH key generator UI
-import QtQuick 2.1
-import QtQuick.Controls 1.0
-import QtQuick.Layouts 1.0
-import QtQuick.Dialogs 1.0
+import QtQuick 2.2
+import QtQuick.Controls 1.1
+import QtQuick.Layouts 1.1
+import QtQuick.Dialogs 1.1
 import com.ics.demo 1.0
 
 ApplicationWindow {
@@ -23,6 +23,7 @@ ApplicationWindow {
     ColumnLayout {
         x: 10
         y: 10
+        anchors.fill: parent
 
         // Key type
         RowLayout {
@@ -98,6 +99,53 @@ ApplicationWindow {
             }
         }
 
+
+        Item {
+            Layout.fillHeight: true
+            Layout.fillWidth: true
+                Rectangle {
+                    id: lineColumn
+                    property int rowHeight: textarea.font.pixelSize + 3
+                    color: "#f2f2f2"
+                    width: 50
+                    height: parent.height
+                    clip: true
+                    Rectangle {
+                        height: parent.height
+                        anchors.right: parent.right
+                        width: 1
+                        color: "#ddd"
+                    }
+                    Column {
+                        y: -textarea.flickableItem.contentY + 4
+                        width: parent.width
+                        Repeater {
+                            model: Math.max(textarea.lineCount + 2, (lineColumn.height/lineColumn.rowHeight) )
+                            delegate: Text {
+                                id: text
+                                color: "#666"
+                                font: textarea.font
+                                width: lineColumn.width
+                                horizontalAlignment: Text.AlignHCenter
+                                verticalAlignment: Text.AlignVCenter
+                                height: lineColumn.rowHeight
+                                renderType: Text.NativeRendering
+                                text: index
+                            }
+                        }
+                    }
+                }
+                TextArea {
+                    id: textarea
+                    objectName: "editor"
+                    anchors.left: lineColumn.right
+                    anchors.right: parent.right
+                    anchors.top: parent.top
+                    anchors.bottom: parent.bottom
+                    wrapMode: TextEdit.NoWrap
+                    frameVisible: false
+                }
+            }
     }
 
     FileDialog {
