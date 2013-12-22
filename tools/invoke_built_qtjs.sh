e68e7c3a5ebd45a8add33bceb572ed838af4ced5
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-12-22 23:14:16 +0000

    more convenient gdb invocation

--- tools/invoke_built_qtjs.sh
+++ tools/invoke_built_qtjs.sh
@@ -1,11 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 MYDIR=$(dirname "$(readlink -f "$0")")
 TOPDIR="$MYDIR/.."
 
 export LD_LIBRARY_PATH="$TOPDIR/lib/cpgf/lib:$TOPDIR/build/src/core:$TOPDIR/build/src/widgets:$TOPDIR/build/src/qml:$TOPDIR/build/src/gui:$TOPDIR/lib/node/deps/v8/out/native/lib.target/"
 
-"$TOPDIR/build/src/runner/qtjs" "$@"
+PRECMD=''
+ARGS=()
+while test "$#" -gt 0 ; do
+    case "$1" in
+        '--gdb')
+            PRECMD="$PRECMD gdb --args"
+            ;;
+        *) ARGS+=("$1")
+            ;;
+    esac
+    shift
+done
+$PRECMD "$TOPDIR/build/src/runner/qtjs" ${ARGS[@]}
 
 exit $?
 
