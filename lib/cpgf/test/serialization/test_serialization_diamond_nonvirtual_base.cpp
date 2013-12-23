271ac63bcd7e23a7917d61a102b95b47b24f82e4
Sarunas Valaskevicius <rakatan@gmail.com>
2013-12-23 20:43:54 +0000

    fix tests crashes

--- lib/cpgf/test/serialization/test_serialization_diamond_nonvirtual_base.cpp
+++ lib/cpgf/test/serialization/test_serialization_diamond_nonvirtual_base.cpp
@@ -152,7 +152,7 @@ void doTestDiamondNonvirtualBase(IMetaStorageWriter * writer, IMetaStorageReader
 
 	GScopedInterface<IMetaArchiveWriter> archiveWriter(createMetaArchiveWriter(service.get(), writer));
 
-	GScopedInterface<IMetaClass> metaClass(service->findClassByName("TestSerializeClassD"));
+	GScopedInterface<IMetaClass> metaClass(service->findClassByName("global.TestSerializeClassD"));
 
 	D instance;
 	instance.B::a = 0x1a;
