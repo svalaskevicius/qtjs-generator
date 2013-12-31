cc712e824b733ca12f56c2616453b2589d7fed47
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2013-12-31 21:28:58 +0000

    optimise and humanise perl preprocessor

--- metagen/gen_pp.sh
+++ metagen/gen_pp.sh
@@ -9,38 +9,84 @@ if ! test -d "$QTINC" ; then
 fi
 
 
-function prepare_file() {
+function prepare_files() {
     perl -e '
-    open FH, @ARGV[0] ;
-    my $s="";
-    while (<FH>) {
-        $s .= $_;
+    use strict;
+    local $| = 1;
+    foreach my $filename (<STDIN>) {
+        $filename =~ s/^\s+//;
+        $filename =~ s/\s+$//;
+        my $s="";
+
+        print "processing $filename..";
+        open (my $fh, $filename);
+        while (my $line = <$fh>) {
+            $s .= $line;
+        }
+        close $fh;
+
+        $s =~ s/Q_DISABLE_COPY\s*\((.*?)\)/\1(const \1 &){} \1 &operator=(const \1 &){}/g;
+        $s =~ s/Q_STATIC_ASSERT(?:_X)?(\((?:[^()]+|(?1))*\))//g;
+        $s =~ s/Q_DECL_NOEXCEPT_EXPR(\((?:[^()]+|(?1))*\))//g;
+        $s =~ s/QT_ASCII_CAST_WARN//g;
+        $s =~ s/Q_WIDGETS_EXPORT//g;
+        $s =~ s/Q_SLOTS//g;
+        $s =~ s/Q_INVOKABLE//g;
+        $s =~ s/Q_SIGNALS://g;
+        $s =~ s/Q_REVISION\s*\(\s*[0-9]+\s*\)//g;
+        $s =~ s/Q_DECLARE_FLAGS\s*\(([^,()]+),\s*([^,()]+)\)/typedef QFlags<\2> \1;/g;
+
+        $s =~ s/class QByteArray;//g;
+        $s =~ s/template<(class|typename)\s*T>\s*class\s*QList;//g;
+        $s =~ s/template<(class|typename)\s*Key,\s*(class|typename)\s*Value> class QMap;//g;
+        $s =~ s/class QString;//g;
+        $s =~ s/class QStringList;//g;
+        $s =~ s/class QVariant;//g;
+
+        $s =~ s/\r/\n/g;
+
+        $s =~ s/
+            (
+                (
+                    [{;}]
+                |
+                    (public|protected|private):
+                |
+                    template[\s\n]*<.*?>
+                )
+                [\s\n]*
+                ((explicit|inline)[\s\n]*)?
+                [a-zA-Z0-9_]+
+                \s*
+                \((.|\n)*?\)
+            )
+            [\s\n]*
+            :
+            (
+                \s*
+                (
+                    (^\s*\#.*$)
+                |
+                    [\s\n,]*
+                    [a-zA-Z0-9_]+
+                    [\s\n]*
+                    \(
+                        [^()]*
+                    \)
+                )
+            )+
+            [\s\n]*
+            \{
+            /\1 {/xgm;
+
+        open my $fh, ">".$filename;
+        print $fh $s;
+        close $fh;
+
+        print ".\n"
+        
     }
-    close FH;
-
-    $s =~ s/Q_DISABLE_COPY\s*\((.*?)\)/\1(const \1 &){} \1 &operator=(const \1 &){}/g;
-    $s =~ s/Q_STATIC_ASSERT(?:_X)?(\((?:[^()]+|(?1))*\))//g;
-    $s =~ s/Q_DECL_NOEXCEPT_EXPR(\((?:[^()]+|(?1))*\))//g;
-    $s =~ s/QT_ASCII_CAST_WARN//g;
-    $s =~ s/Q_WIDGETS_EXPORT//g;
-    $s =~ s/Q_SLOTS//g;
-    $s =~ s/Q_INVOKABLE//g;
-    $s =~ s/Q_SIGNALS://g;
-    $s =~ s/Q_REVISION\s*\(\s*[0-9]+\s*\)//g;
-    $s =~ s/Q_DECLARE_FLAGS\s*\(([^,()]+),\s*([^,()]+)\)/typedef QFlags<\2> \1;/g;
-
-    $s =~ s/class QByteArray;//g;
-    $s =~ s/template<(class|typename)\s*T>\s*class\s*QList;//g;
-    $s =~ s/template<(class|typename)\s*Key,\s*(class|typename)\s*Value> class QMap;//g;
-    $s =~ s/class QString;//g;
-    $s =~ s/class QStringList;//g;
-    $s =~ s/class QVariant;//g;
-
-    $s =~ s/(([{;}]|(public|protected|private):|template[\s\n]*<[^<>]*>)[\s\n]*[a-zA-Z0-9_]+\(.*?\))[\s\n]*:([\s\n]*(\n\s*#[^\n]*\n)?[\s\n]*[a-zA-Z0-9_]+[\s\n]*\([^)]*\)[\s\n]*,?)+\{[^{}]*\}/\1 {}/gms;
-
-    open FH, ">".@ARGV[0];
-    print FH $s;
-    close FH;' "$1"
+    '
 }
 
 rm -Rf qtheaders xml build
@@ -55,11 +101,7 @@ rm -R QtCLucene QtConcurrent QtDBus QtDeclarative QtDesigner QtDesignerComponent
       QtWebKitWidgets QtXml QtXmlPatterns QtZlib
 popd
 
-for f in $(find qtheaders -type f) ; do
-    prepare_file "$f"
-    echo -n .
-done
-echo ""
+find qtheaders -type f | prepare_files
 
 doxygen qt.doxyfile
 
