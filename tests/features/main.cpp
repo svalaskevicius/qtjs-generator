90d0d14d0ae1cd26270f776dae936652cdfca1d7
Sarunas Valaskevicius <rakatan@gmail.com>
2014-01-01 16:13:59 +0000

    add global QApplication for tests

--- tests/features/main.cpp
+++ tests/features/main.cpp
@@ -1,5 +1,28 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+#include <QApplication>
+#include "src/eventdispatcherlibuv.h"
+
+struct GlobalInitialiser {
+
+    qtjs::EventDispatcherLibUv *ev_dispatcher;
+    QCoreApplication *app;
+
+    GlobalInitialiser() {
+       ev_dispatcher = new qtjs::EventDispatcherLibUv();
+       QCoreApplication::setEventDispatcher(ev_dispatcher);
+       int argc = 0;
+       char *argv[1] = {0};
+       app = new QApplication(argc, argv, 0);
+    }
+
+    ~GlobalInitialiser() {
+        delete app;
+    }
+
+} global;
+
+
 #include "features/eventDispatcher.h"
 
