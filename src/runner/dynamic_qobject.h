9834271465f305c040826db7dc8431d44a09d4cc
Sarunas Valaskevicius <svalaskevicius@inviqa.com>
2014-01-17 20:50:09 +0000

    move out callinfo class to its file

--- src/runner/dynamic_qobject.h
+++ src/runner/dynamic_qobject.h
@@ -15,13 +15,7 @@
 
 namespace qtjs_binder {
 
-struct CallInfo {
-    QVector<int> parameterTypeIds;
-    int returnTypeId;
-    cpgf::IScriptFunction * callback;
-    void invoke(void **);
-    ~CallInfo();
-};
+struct CallInfo;
 
 class DLL_PUBLIC QtSignalConnector : public QObject {
 public:
