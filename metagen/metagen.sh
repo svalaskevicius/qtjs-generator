7ba331774a761a5bd665881c86b58db6dad0c96c
Sarunas Valaskevicius <rakatan@gmail.com>
2014-07-13 17:52:13 +0100

    generalise bindings generator

--- metagen/metagen.sh
+++ metagen/metagen.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-QTINC="$( readlink -f "$1" )"
-
-if ! test -d "$QTINC" ; then
-    echo "qt dir not specified." >&2
-    exit 1
-fi
-
 java -cp "../lib/cpgf/tools/metagen/tool/bin:../lib/cpgf/tools/metagen/js.jar" metagen --config qtCore_cfg.js --xml xml/index.xml
 java -cp "../lib/cpgf/tools/metagen/tool/bin:../lib/cpgf/tools/metagen/js.jar" metagen --config qtWidgets_cfg.js --xml xml/index.xml
 java -cp "../lib/cpgf/tools/metagen/tool/bin:../lib/cpgf/tools/metagen/js.jar" metagen --config qtQml_cfg.js --xml xml/index.xml
 java -cp "../lib/cpgf/tools/metagen/tool/bin:../lib/cpgf/tools/metagen/js.jar" metagen --config qtGui_cfg.js --xml xml/index.xml
-
-find build/ -type f -name '*.cpp' -exec perl -p -i -e "s#\"$QTINC/?(.*?)\"#<\1>#g" '{}' \;
 
